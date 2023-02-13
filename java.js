ac163626ae83df98ca221f8b4cf9fd16c8a073ce
fernandojuan95 <fernando.nicolas.juan@hotmail.com>
2023-02-13 11:13:39 -0300

    se agrega el proyecto

---
+++ java.js
@@ -0,0 +1,33 @@
+
+function PreciodeProducto(){
+    let librofrankenstein = 4000;
+    let librosouleater = 1500;
+    let libroelcuervo = 2000;
+}
+function libros() {
+    let PreciodeProducto 
+
+    switch (libros) {
+        case "frankenstein":
+            let PreciodeProducto = librofrankenstein;
+            break;
+        case "souleater":
+            PreciodeProducto = librosouleater;
+        case "elcuervo":
+            PreciodeProducto = libroelcuervo;
+        break;
+        default:
+            PreciodeProducto = false;
+        break;
+}
+}
+function CantidadDelibros(){
+    if (!Cantidad  <= 3  || Cantidad  >= 4  || Cantidad >=10 )
+        Cantidad = false;
+    }
+
+    
+    CantidadDelibros()
+
+
+    return precio;
